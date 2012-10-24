29a6ba983e9884906c163d718cb8712f2e9d4baf
Atsushi Kataoka <atsushi.kataoka@msmc.jp>
2012-10-24 09:25:03 +0900

    Fixed bug and modified example
    *added security framework for module.xcconfig
    *example app that uses developer token

--- ios_module/example/app.js
+++ ios_module/example/app.js
@@ -22,18 +22,30 @@ Ti.API.info(evernote.Types.EDAM_NOTE_SOURCE_MAIL_CLIP);
 Ti.API.info(evernote.Types.PrivilegeLevel);
 Ti.API.info(evernote.Types.PrivilegeLevel.SUPPORT);
 
+// You can get developer token for sandbox:
+// https://sandbox.evernote.com/api/DeveloperToken.action
+// You can get developer token for production:
+// https://www.evernote.com/api/DeveloperToken.action
+var developerToken = '<YOUR DEVELOPER TOKEN HERE>';
 
 var userstore = evernote.createUserStoreClient(config.url+"user");
-userstore.authenticate(config.demo.email, config.demo.password, config.consumerKey, config.consumerSecret, function(event) {
-	if(event.type == 'success') {
-		Ti.API.info(event.result.toHash());
 
-		var notestore = evernote.createNoteStoreClient(config.url+"note/"+event.result.user.shardId);
+userstore.getNoteStoreUrl(developerToken, function(event) {
+	if(event.type == 'success') {
+		var notestore = evernote.createNoteStoreClient(event.result);
 		
-		var notes = notestore.listNotebooks(event.result.authenticationToken, function(event2) {
+		var notes = notestore.listNotebooks(developerToken, function(event2) {
 			table.data = event2.result.map(function(note){ return({title: note.name}); });
 			
 			var note = evernote.createNote({title: "Test"});
+
+			var content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+			content = content +"<!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml.dtd\">";
+			content = content +"<en-note>";
+			content = content +"This note is craeted for testing.";
+			content = content +"</en-note>";
+			note.content = content;
+			
 			note.tagNames = ['tag1', 'tag2'];
 			
 			var data = evernote.createData();
@@ -52,8 +64,8 @@ userstore.authenticate(config.demo.email, config.demo.password, config.consumerK
 			res.attributes = attrs;
 			
 			note.resources = [res];
-			
-			notestore.createNote(event.result.authenticationToken, note, function(event) {
+
+			notestore.createNote(developerToken, note, function(event) {
 				Ti.API.info(event);
 				if(event.type == 'success') {
 					alert("Create new note");
